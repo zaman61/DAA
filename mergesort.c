7e8f54736736191d2c8d9809d8ce77607dbbd8e3
zaman61 <mz8923567@gmail.com>
2025-04-09 17:25:02 +0530

    Create mergesort.c

---
+++ mergesort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void merge(int arr[], int l, int m, int r) {
+    int i, j, k, n1 = m - l + 1, n2 = r - m;
+    int L[n1], R[n2];
+    for (i = 0; i < n1; i++) L[i] = arr[l + i];
+    for (j = 0; j < n2; j++) R[j] = arr[m + 1 + j];
+    for (i = 0, j = 0, k = l; i < n1 && j < n2; k++) arr[k] = (L[i] <= R[j]) ? L[i++] : R[j++];
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+}
+
+void mergeSort(int arr[], int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7}, n = sizeof(arr) / sizeof(arr[0]);
+    mergeSort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    return 0;
+}
